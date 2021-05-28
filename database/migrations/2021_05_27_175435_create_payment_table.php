1389f0ee2a72e964454f099a07e8e621f0c1abeb
Muhammad Ahmed <mian768@gmail.com>
2021-05-28 18:12:21 +0500

    alpha push

---
+++ database/migrations/2021_05_27_175435_create_payment_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('payment');
+            $table->string('payment_source');
+            $table->integer('amount');
+            $table->integer('total_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment');
+    }
+}
